81a3ed1edace96736048efd85f538f69d0aa18c4
Hatim <hatimjuzer78652@gmail.com>
2021-09-10 12:25:37 +0500

    Added crud endpoints

--- JWTTutorial/settings.py
+++ JWTTutorial/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
+    'languages.apps.LanguagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
